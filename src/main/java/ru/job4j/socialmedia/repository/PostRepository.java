96389159438775e922b85bc7be4da544419b9860
SergeyDevStart <vasenev.front@gmail.com>
2025-03-21 12:06:24 +0700

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
@@ -1,7 +1,17 @@
 package ru.job4j.socialmedia.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.ListCrudRepository;
 import ru.job4j.socialmedia.model.Post;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface PostRepository extends ListCrudRepository<Post, Long> {
+    List<Post> findByUserId(Long id);
+
+    List<Post> findByCreatedGreaterThanEqualAndCreatedLessThanEqual(LocalDateTime startAt, LocalDateTime endAt);
+
+    Page<Post> findByOrderByCreatedDesc(Pageable pageable);
 }
